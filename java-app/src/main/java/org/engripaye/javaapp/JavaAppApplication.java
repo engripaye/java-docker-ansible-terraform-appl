c4ba0abc94d2a96a8001ff87ec8805c6e31084ce
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-17 20:06:55 +0100

    first commit

---
+++ java-app/src/main/java/org/engripaye/javaapp/JavaAppApplication.java
@@ -0,0 +1,20 @@
+package org.engripaye.javaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAppApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
